eb01b2b97c2df77263b846c8c1c9fbdc2033681f
Ilya Baykov <ily.boykov@x5.ru>
2022-01-31 16:13:24 +0300

    Initial Commit

---
+++ Loyalty/LoyaltyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoyaltyApp.swift
+//  Loyalty
+//
+//  Created by Илья Бойков on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct LoyaltyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
